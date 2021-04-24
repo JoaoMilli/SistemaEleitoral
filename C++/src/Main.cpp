01428dbdc25aab34bd64d63ac8bf32d8460b5799
MillJoao <pedromilli123@gmail.com>
2021-04-23 23:49:35 -0300

    Main com leitura de CSV

---
+++ C++/src/Main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void leCSV(string path){
+
+    ifstream file (path);
+
+    if (!file.is_open()){
+        std::cout << "Erro: Não foi possível abrir o arquivo" << "\n";
+        return;
+    }
+
+    string numero;
+    string votos_nominais;
+    string situacao;
+    string nome;
+    string nome_urna;
+    string sexo;
+    string data_nasc;
+    string destino_voto;
+    string numero_partido;
+
+    getline(file, numero);
+
+    while(getline(file, numero, ',')){
+
+        getline(file, votos_nominais, ',');
+        getline(file, situacao, ',');
+        getline(file, nome, ',');
+        getline(file, nome_urna, ',');
+        getline(file, sexo, ',');
+        getline(file, data_nasc, ',');
+        getline(file, destino_voto, ',');
+        getline(file, numero_partido);
+
+        cout << " " << numero << " " << votos_nominais << " " << situacao << " " << nome << " " << nome_urna << " " << sexo << " " << data_nasc << " " << destino_voto << " " << numero_partido << " " << "\n";
+    }
+
+    file.close();
+}
+
+int main(){
+
+    string path = "src/candidatos.csv";
+    leCSV(path);
+
+}
